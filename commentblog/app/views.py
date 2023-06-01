0d3f6c8efb98b4d980843016821476608ef09998
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-06-01 21:05:40 +0900

    230601 commentblog 기능 완성

--- commentblog/app/views.py
+++ commentblog/app/views.py
@@ -29,3 +29,21 @@ def detail(request, post_pk):
         )
 
     return render(request, 'detail.html', {'post':post})
+
+def edit(request, post_pk):
+    post=Post.objects.get(pk=post_pk)
+
+    if request.method=='POST':
+        Post.objects.filter(pk=post_pk).update(
+            title=request.POST['title'],
+            content=request.POST['content']
+        )
+        return redirect('detail',post_pk)
+    return render(request, 'edit.html', {'post':post})
+
+def delete(request, post_pk):
+    # 따로 delete는 html 파일이 필요 없다
+    post = Post.objects.get(pk=post_pk)
+    post.delete()
+
+    return redirect('home')
